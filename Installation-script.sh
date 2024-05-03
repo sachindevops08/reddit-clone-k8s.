a8040d56019ca8db8245b7ac7b4e2081fb27a7a7
sachin08 <131578520+sachindevops08@users.noreply.github.com>
2024-05-03 13:02:41 +0530

    Update Installation-script.sh

--- Installation-script.sh
+++ Installation-script.sh
@@ -29,9 +29,9 @@ sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 sudo apt-get update -y
 sudo apt-get install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
 #sudo apt install docker.io -y
-#sudo usermod -aG docker ubuntu
-#newgrp docker
-#sudo chmod 777 /var/run/docker.sock
+sudo usermod -aG docker ubuntu
+newgrp docker
+sudo chmod 777 /var/run/docker.sock
 #docker version
 
 # Install Trivy
