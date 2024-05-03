86df627568cd600a11b2612b7e0ee674652301d2
sachin08 <131578520+sachindevops08@users.noreply.github.com>
2024-05-03 12:38:26 +0530

    Update Installation-script.sh

--- Installation-script.sh
+++ Installation-script.sh
@@ -18,11 +18,21 @@ sudo systemctl status jenkins
 sudo cat /var/lib/jenkins/secrets/initialAdminPassword
 
 #Install docker
-sudo apt install docker.io -y
-sudo usermod -aG docker ubuntu
-newgrp docker
-sudo chmod 777 /var/run/docker.sock
-docker version
+sudo apt-get update -y
+sudo apt-get install -y ca-certificates curl
+sudo install -m 0755 -d /etc/apt/keyrings
+sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
+sudo chmod a+r /etc/apt/keyrings/docker.asc
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
+$(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
+sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo apt-get update -y
+sudo apt-get install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+#sudo apt install docker.io -y
+#sudo usermod -aG docker ubuntu
+#newgrp docker
+#sudo chmod 777 /var/run/docker.sock
+#docker version
 
 # Install Trivy
 sudo apt-get install wget apt-transport-https gnupg lsb-release -y
